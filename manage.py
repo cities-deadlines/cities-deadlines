31285a6e96621f3b405ee2d1bec4fe4368822f17
Alex Langshur <adlangshur@gmail.com>
2020-02-04 19:32:50 -0800

    Updated generic views

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
+"""
+Django's command-line utility for administrative tasks.
+"""
+
 import os
 import sys
 
-
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cities_backend.settings')
-    try:
-        from django.core.management import execute_from_command_line
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    try: from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -15,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
