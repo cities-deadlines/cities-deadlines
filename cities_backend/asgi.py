ea16454a6c8b91df92fcd9fc8d4c7a676ff3cf8f
Alex Langshur <adlangshur@gmail.com>
2020-02-04 18:13:58 -0800

    Added Django settings

--- cities_backend/asgi.py
+++ cities_backend/asgi.py
@@ -1,10 +1,6 @@
 """
 ASGI config for cities_backend project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
