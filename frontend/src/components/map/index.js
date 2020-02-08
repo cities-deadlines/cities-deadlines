2d36f1572546f87308ed855b797142c3ba316e6c
Harry Mellsop <harrymellsop@gmail.com>
2020-02-07 17:17:58 -0800

    Konva mapping environment configured, now resizes with the circles drawn randomly; will work on making the content more logical

--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -4,18 +4,82 @@ import React, { Component } from "react";
 class MapModule extends Component {
     constructor(props) {
         super(props);
-        console.log("Hello world");
-        console.log("Reeee");
     }
 
     render() {
-        return (
-            <canvas id='mapCanvas' style={{
+        // <script type="text/javascript" src="./frontend/src/components/map/map-controller.js"></script>
+        /*
+        <canvas id='mapCanvas' style={{
                 right: '0',
                 width: '100%',
                 height: '100%'
             }}></canvas>
+        */
+        
+        return (
+            <div id="map-container"style={{
+                right: '0',
+                width: '100%',
+                height: '100%'
+            }}></div>
         );
+    }
+
+    componentDidMount() {
+
+        console.log("THIERRY HENRY");
+        
+        var aScript = document.createElement('script');
+        aScript.type = 'text/javascript';
+        aScript.src = "https://unpkg.com/konva@4.1.3/konva.min.js";
+    
+        document.head.appendChild(aScript);
+
+        aScript.onload = function() {
+
+            var width = document.getElementById('map-container').offsetWidth;
+            var height = document.getElementById('map-container').offsetHeight;
+
+            console.log(width);
+            console.log(height);
+
+            var stage = new Konva.Stage({
+                container: 'map-container',
+                width: width,
+                height: height,
+                draggable: true,
+                id: "map-konva-layer"
+            });
+
+            var layer = new Konva.Layer();
+            stage.add(layer);
+
+            function resizeStage() {
+                stage.height(document.getElementById('map-container').offsetHeight);
+                stage.width(document.getElementById('map-container').offsetWidth);
+            }
+
+            window.addEventListener("resize", resizeStage);
+
+            var WIDTH = 3000;
+            var HEIGHT = 3000;
+            var NUMBER = 200;
+
+            function generateNode() {
+                return new Konva.Circle({
+                    x: WIDTH * Math.random(),
+                    y: HEIGHT * Math.random(),
+                    radius: 50,
+                    fill: 'red',
+                    stroke: 'black'
+                });
+            }
+
+            for (var i = 0; i < NUMBER; i++) {
+                layer.add(generateNode());
+            }
+            layer.draw();
+        };
     }
 }
 
