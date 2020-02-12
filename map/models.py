a125621385cbccf3a5d174725483e97ef74947c3
Harry Mellsop <harrymellsop@gmail.com>
2020-02-12 13:53:27 -0800

    Laying out concepts for hierarchy

--- map/models.py
+++ map/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-import json
 
 # Create your models here.
 class MapTile(models.Model):
     xCoord = models.IntegerField()
     yCoord = models.IntegerField()
     # store the contents of the maptile in json format; provides easy formatting for limited data size
-    mapContentsJson = models.TextField()
+    
+    # so, we need some way to encapsulate buildings at given locations on the map, roading/filler tile
+    # networks.  On the constructor, needs to construct 100 buildings under properties, give ownership
+    # to some fake account, etc.
 
     def __str__(self):
         return "MapTile"
