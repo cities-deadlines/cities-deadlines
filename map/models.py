c7c64ccae59b00b7ffce3b0bc7254a03644525dd
Harry Mellsop <harrymellsop@gmail.com>
2020-02-12 13:19:32 -0800

    MapTile gettings there; heading to class now

--- map/models.py
+++ map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MapTile(models.Model):
+
+    xCoord = models.IntegerField()
+    yCoord = models.IntegerField()
+
+    def __str__(self):
+        return "MapTile"
