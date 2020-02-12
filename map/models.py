1cfa7325de48aa380ac365bb700d563a9b43d1cd
Harry Mellsop <harrymellsop@gmail.com>
2020-02-12 13:48:15 -0800

    Properties linkage

--- map/models.py
+++ map/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import json
 
 # Create your models here.
 class MapTile(models.Model):
 
     xCoord = models.IntegerField()
     yCoord = models.IntegerField()
+    # store the contents of the maptile in json format; provides easy formatting for limited data size
+    mapContentsJson = models.TextField()
 
     def __str__(self):
         return "MapTile"
