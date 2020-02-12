c8f6231ee05d696f36bbe94ca74598c599833192
Harry Mellsop <harrymellsop@gmail.com>
2020-02-12 15:00:54 -0800

    Constraint logic implemented

--- properties/models.py
+++ properties/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from map.models import MapTile
+from user.models import User
 
 # Create your models here.
 class Building(models.Model):
-    owner = models.ForeignKey()
+
+    class BuildingTypes(models.TextChoices):
+        skyscraper1 = "skyscraper1"
+        skyscraper2 = "skyscraper2"
+        tripletowers1 = "tripletowers1"
+
+    # current value - we should keep all currency in integer amounts, to stop overflow issues with percentages.
     value = models.IntegerField()
+
+    # allow only the specific building types as defined above
+    buildingType = models.CharField(choices=BuildingTypes.choices)
+    
+    # need some way to store purchase history here; realistically, a purchase history is probably going to
+    # be its own data structure?
+
+    # instantiate the many-to-one relationship between users and buildings (a user can own multiple buildings)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
